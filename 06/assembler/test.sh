18c9e081cd364cf9738aafc72c9ac3c2f7ba9591
kentakuramochi <soratobi93@gmail.com>
2021-01-10 23:59:50 +0900

    fix comparing process

--- 06/assembler/test.sh
+++ 06/assembler/test.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+# assembler
 HASM_REF=../../../tools/Assembler.sh
-HASM=./bin/asm
+HASM_DIR=./bin
+HASM=${HASM_DIR}/asm
 
 test_asm() {
-    HACK=${1/.asm/.hack}
+    SRC_DIR=$(dirname $1)
+    HACK_BASE=$(basename ${1/.asm/.hack})
 
+    HACK_REF=${HASM_DIR}/${HACK_BASE/.hack/_ref.hack}
+    HACK=${HASM_DIR}/${HACK_BASE}
+
+    # reference output
     ${HASM_REF} $1
-    mv ${HACK} ./bin
+    mv ${SRC_DIR}/${HACK_BASE} ${HACK_REF}
 
+    # output
     ${HASM} $1
-    mv ${HACK} ./bin
+    mv ${SRC_DIR}/${HACK_BASE} ${HASM}
 
-    diff ./bin/$(basename ${HACK}) ${HACK}
+    # compare
+    diff ${HACK_REF} ${HACK}
 }
 
-# test for no label
 test_asm ../add/Add.asm
 test_asm ../max/MaxL.asm
 
